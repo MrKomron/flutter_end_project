3ea1b576454ad906e6077288c935012a1d1086a1
Komron <r0899442@student.thomasmore.be>
2023-03-05 03:32:50 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+import 'team.dart';
+import 'settings.dart';
+import 'login.dart';
+import 'contact.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: QuizApp(),
+    );
+  }
+}
+
+
+
+class QuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 5,
+        child: Scaffold(
+            appBar: AppBar(
+
+              title: Image.asset("assets/images/logo.png", scale: 4,),
+
+            ),
+            bottomNavigationBar: TabBar(
+                indicatorColor: Colors.blueAccent,
+                labelColor: Colors.blueAccent,
+                tabs: [
+                  Tab(icon:Icon(Icons.home, size: 35,)),
+                  Tab(icon:Icon(Icons.people, size: 35,)),
+                  Tab(icon:Icon(Icons.phone, size: 35,)),
+                  Tab(icon:Icon(Icons.logout, size: 35,)),
+                  Tab(icon:Icon(Icons.settings, size: 35,)),
+
+                ]
+            ),
+            body: TabBarView(
+              children: [
+                Home(),
+                Team(),
+                Contact(),
+                Login(),
+                Settings(),
+
+              ],
+            )
+        )
+    );
+  }
+}
