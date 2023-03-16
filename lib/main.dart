9ca4ac91de22a89a19ee9867de72517096cf9a29
Komron <r0899442@student.thomasmore.be>
2023-03-16 22:05:43 +0100

    home page is aangemaakt

--- lib/main.dart
+++ lib/main.dart
@@ -79,6 +79,8 @@ class MedicalCenter extends StatelessWidget {
                 Contact(),
                 Settings(),
               ],
-            )));
+            )
+        )
+    );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,9 +5,131 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Home')),
+        title: Text('Hospital Name'),
       ),
-      body: SingleChildScrollView(
+      body: Column(
+        children: [
+          Container(
+            height: 200.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/hospital.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(
+              child: Text(
+                'Welcome to our Hospital',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 26.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Our Services',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 1,
+                      crossAxisSpacing: 10.0,
+                      mainAxisSpacing: 10.0,
+                      children: [
+                        _buildServiceItem(
+                          context,
+                          Icons.healing,
+                          'Orthopedics',
+                          'Our orthopedic specialists are experts in the treatment of bone and joint disorders, from sports injuries to arthritis.',
+                        ),
+                        _buildServiceItem(
+                          context,
+                          Icons.favorite,
+                          'Cardiology',
+                          'Our cardiology department is staffed by highly trained professionals who provide expert care for a range of heart conditions, from arrhythmia to heart attack.',
+                        ),
+                        _buildServiceItem(
+                          context,
+                          Icons.content_paste,
+                          'Laboratory',
+                          'We provide a full range of laboratory services, from routine blood tests to complex genetic testing.',
+                        ),
+                        _buildServiceItem(
+                          context,
+                          Icons.local_hospital,
+                          'Emergency',
+                          'Our emergency department is open 24/7 to provide immediate care for life-threatening injuries and illnesses.',
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildServiceItem(BuildContext context, IconData icon, String title, String description) {
+    return GestureDetector(
+      onTap: () {
+        // TODO: navigate to service page
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 48.0,
+              color: Theme.of(context).primaryColor,
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 5.0),
+            Text(
+              description,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14.0,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
