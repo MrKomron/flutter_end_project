ae5a6f760204147fcab77f74bb54c1d14673156e
Komron <r0899442@student.thomasmore.be>
2023-03-17 00:03:04 +0100

    extra contact gegevens

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -52,8 +52,45 @@ class _ContactPageState extends State<ContactPage> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              SizedBox(height: 16.0),
+          Text(
+            'Phone',
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            '+1 555-1234-567',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            'Website',
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8.0),
+          InkWell(
+            child: Text(
+              'www.example.com',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+            onTap: () => _launchURL(),
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            'Send us a message',
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16.0),
+
+        Form(
             key: _formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -131,8 +168,17 @@ class _ContactPageState extends State<ContactPage> {
               ],
             ),
           ),
-        ),
+        ],),
+      ),
       ),
     );
+  }
+}
+_launchURL() async {
+  const url = 'https://www.example.com';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,14 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Settings')),
+        title: Text('Contact Us'),
       ),
-      body: SingleChildScrollView(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 16.0),
+            Text(
+              'Phone',
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              '+1 555-1234-567',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Website',
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            InkWell(
+              child: Text(
+                'www.example.com',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+              onTap: () => _launchURL(),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  _launchURL() async {
+    const url = 'https://www.example.com';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
