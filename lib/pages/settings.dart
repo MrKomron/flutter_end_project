4a567136b6d81428ef16eba0ced4c6b76e7e07dc
Komron <r0899442@student.thomasmore.be>
2023-03-17 02:15:24 +0100

    audioplayers code is togevoegd

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-class Settings extends StatelessWidget {
-  @override
+void main() {
+  runApp(AppRoot());
+}
+
+class AppRoot extends StatelessWidget {
+  Widget build(BuildContext buildContext) => MaterialApp(
+    home: Scaffold(
+      body: AppTree(),
+      appBar: AppBar(title: Text("Titel van de app"),),
+    ),
+  );
+}
+
+class AppTree extends StatelessWidget {
+  final AudioPlayer audioSpeler = AudioPlayer();
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Settings')),
-      ),
-      body: SingleChildScrollView(
-      ),
+    return Center(
+        child: ElevatedButton(
+            onPressed: () { audioSpeler.play(AssetSource('audio/holiday-yuriybespalov.mp3'), volume: 1); },
+            child: Text('Speel muziek', textScaleFactor: 2)
+        )
     );
   }
 }
